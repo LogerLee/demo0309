e5de4dffd13a034807dd339ea7ce52ef3a11e38b
李硕成 <xiguatailanggx@163.com>
2020-03-09 20:20:53 +0800

    主页底端导航栏的创建+悬浮按钮

--- app/src/main/java/com/loger/topgear/demo0309/MainActivity.java
+++ app/src/main/java/com/loger/topgear/demo0309/MainActivity.java
@@ -1,13 +1,42 @@
 package com.loger.topgear.demo0309;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+@Viewinject(mainlayoutid = R.layout.activity_main)
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.fac_main_home)
+    FloatingActionButton facMainHome;
+    @BindView(R.id.rb_main_s)
+    RadioButton rbMainS;
+    @BindView(R.id.rb_main_o)
+    RadioButton rbMainO;
+    @BindView(R.id.rb_main_l)
+    RadioButton rbMainL;
+    @BindView(R.id.rb_main_e)
+    RadioButton rbMainE;
+    @BindView(R.id.rg_main_top)
+    RadioGroup rgMainTop;
+    @BindView(R.id.fl_main_bottom)
+    FrameLayout flMainBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+    }
+
+
+    @OnClick(R.id.fac_main_home)
+    public void onViewClicked() {
     }
 }
